abac921baf820a4445cc45e3ad12fd848da8b479
Bethel <146152877+Bethellu@users.noreply.github.com>
2023-10-02 11:13:33 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+let stopTime = 0
+let counting = true
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 5 5 5 5 5 5 . . . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . . . 5 5 5 5 5 5 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.setStayInScreen(true)
+mySprite.left = 0
+mySprite.fx = 30
+mySprite.vx = 90
+for (let index = 0; index < 3; index++) {
+    mySprite.vx = 80
+    pause(1000)
+}
+/**
+ * per 100/1000 ms update.
+ * 
+ * stoptime per 1/10ms.
+ */
+game.onUpdateInterval(100, function () {
+    if (counting) {
+        stopTime += 0.1
+        if (mySprite.vx == 0) {
+            counting = false
+            mySprite.sayText("" + stopTime + "sec", 5000, false)
+        }
+    }
+})
