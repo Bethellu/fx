265508c2c83c44d217fe50a0c410831a7c8c1bce
Bethel <146152877+Bethellu@users.noreply.github.com>
2023-10-11 10:59:19 +0800

    Update pxt.json, main.ts, main.blocks, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-let stopTime = 0
-let counting = true
-let mySprite = sprites.create(img`
+stopTime = 0
+counting = True
+mySprite = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . 5 5 5 5 5 5 . . . . . 
@@ -17,32 +17,28 @@ let mySprite = sprites.create(img`
             . . . . . 5 5 5 5 5 5 . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . .
-    `, SpriteKind.Player)
-mySprite.setStayInScreen(true)
+    """),
+    SpriteKind.player)
+mySprite.set_stay_in_screen(True)
 mySprite.left = 0
 mySprite.fx = 30
 mySprite.vx = 90
-for (let index = 0; index < 3; index++) {
+for index in range(3):
     mySprite.vx = 80
     pause(1000)
-}
-/** 
+"""
 
 per 100/1000 ms update.
 
 stoptime per 1/10ms.
 
+"""
 
- */
-game.onUpdateInterval(100, function on_update_interval() {
-    
-    if (counting) {
+def on_update_interval():
+    global stopTime, counting
+    if counting:
         stopTime += 0.1
-        if (mySprite.vx == 0) {
-            counting = false
-            mySprite.sayText("" + ("" + stopTime) + "sec", 5000, false)
-        }
-        
-    }
-    
-})
+        if mySprite.vx == 0:
+            counting = False
+            mySprite.say_text("" + str(stopTime) + "sec", 5000, False)
+game.on_update_interval(100, on_update_interval)
